b110ccbf7cc2517fcb879b4cffed12c487ec34d3
Dario Fabbri <dar.fabbri@gmail.com>
2014-05-10 20:26:39 +0200

    Progressing on the form view.

--- public/app/main.js
+++ public/app/main.js
@@ -22,19 +22,59 @@ require.config({
 requirejs([
 	'jquery',
 	'underscore',
-	'views/login/loginview',
+	'backbone',
+	'text!templates/form-message.html',
 
 	'jquery-ui'
 ],
-function ($, _, LoginView) {
+function ($, _, Backbone, messageHtml) {
+
+	// Customize underscore's templates to mimic mustache style
+	// field replacing.
+	//
+	_.templateSettings = {
+		interpolate: /\{\{(.+?)\}\}/g
+	};
+
+	// Create an extend version of Backbone.View dedicated to form
+	// building.
+	//
+	Backbone.FormView = Backbone.View.extend({
+
+		fldDelay: 1000,
+
+		// Add a generic function to show a message in the form.
+		//
+		showMessage: function(title, message, removeTimeout) {
+
+			// If no remove timeout has been specified, just
+			// set a default to 5 seconds.
+			//
+			removeTimeout = removeTimeout || 5000;
+
+			var html = _.template(messageHtml, { title: title, message: message});
+			this.$('.row').first().before(html).prev().hide().fadeIn(function() {
+				var that = this;
+
+				// A negative remove timeout disables the automatic
+				// removal of the message.
+				//
+				if(removeTimeout >= 0) {
+					setTimeout(function() {
+						$(that).fadeOut(function() {
+							this.remove();
+						});
+					}, removeTimeout);
+				}
+			});
+		}
+	});
 
 	$(document).ready(function() {
 
-		_.templateSettings = {
-			interpolate: /\{\{(.+?)\}\}/g
-		};
-
-		var loginView = new LoginView();
-		$(document.body).append(loginView.render().el);
+		require(['views/login/loginview'], function(LoginView) {
+			var loginView = new LoginView();
+			$(document.body).append(loginView.render().el);
+		});
 	});
 });
--- public/app/views/login/loginview.js
+++ public/app/views/login/loginview.js
@@ -2,17 +2,17 @@ define([
 	'jquery',
 	'underscore',
 	'backbone',
-	'text!templates/login.html',
-	'text!templates/form-message.html'
+	'text!templates/login.html'
 ],
-function ($, _, Backbone, loginHtml, messageHtml) {
+function ($, _, Backbone, loginHtml) {
 	
-	var LoginView = Backbone.View.extend({
+	var LoginView = Backbone.FormView.extend({
 
 		template: _.template(loginHtml),
 
 		events: {
-			'click #login': 'doLogin'
+			'click #login': 'onLoginClick',
+			'keypress input': 'onKeypress'
 		},
 
 		render: function () {
@@ -21,40 +21,62 @@ function ($, _, Backbone, loginHtml, messageHtml) {
 
 			this.$('#login').button();
 
-			_.defer(function() {
+			_.defer(function () {
 				this.$('#username').focus();
 			});
 
 			return this;
 		},
 
+		onKeypress: function (e) {
+			if(e.keyCode == 13) {
+				this.doLogin();
+			}
+		},
+
+		onLoginClick: function () {
+			this.doLogin();
+		},
+
 		doLogin: function () {
 
 			var username,
-				password;
+				password,
+				firstInError;
 
 			// Read username and password fields.
 			//
 			username = this.$('#username').val();
 			password = this.$('#password').val();
 
-			// Both must be present.
+			// Clear previous validation errors from form fields.
 			//
-			if(!username) {
-				this.$('username').addClass('error', 1000);
+			this.resetFieldErrors();
 
-				var html = _.template(messageHtml, { title: 'Errore', message: 'Il campo username è obbligatorio.'});
-				this.$('.row').first().before(html).prev().hide().fadeIn(function() {
-					var that = this;
-					setTimeout(function() {
-						$(that).fadeOut(function() {
-							this.remove();
-						});
-					}, 5000);
-				});
-
+			// Validate username field.
+			//
+			if (!username) {
+				firstInError = firstInError || this.$('#username').addClass('field-error', this.fldDelay);
+				this.showMessage('Errore', 'Il campo username è obbligatorio.');
 			}
+
+			// Validate password field.
+			//
+			if (!password) {
+				firstInError = firstInError || this.$('#password').addClass('field-error', this.fldDelay).focus();
+				this.showMessage('Errore', 'Il campo password è obbligatorio.');
+			}
+
+			// Set the focus on the first field in error.
+			//
+			firstInError && firstInError.focus();
+		},
+
+		resetFieldErrors: function () {
+			this.$('#username').removeClass('field-error');
+			this.$('#password').removeClass('field-error');
 		}
 	});
+
 	return LoginView;
 });
