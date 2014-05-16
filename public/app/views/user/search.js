54c49bc400c04d146037b1bb85baccbaf86e1da4
Dario Fabbri <dar.fabbri@gmail.com>
2014-05-17 00:04:58 +0200

    Added support for persistent search form state.

--- public/app/views/user/search.js
+++ public/app/views/user/search.js
@@ -17,6 +17,100 @@ function ($, _, Backbone, ParentView, userSearch, loginInfo, searchHtml) {
 			'keyup input': 'onKeyup'
 		},
 
+		render: function () {
+			var html = this.template();
+			this.$el.html(html);
+
+			// Set up search form buttons.
+			//
+			this.$('#new').button();
+
+			// Restore form state.
+			//
+			this.searchModelToForm();
+
+			// Configure data table.
+			//
+			var datatable = this.$('#list').dataTable({
+				serverSide: true,
+				ajax: this.ajaxSearch,
+				dom: 'tipr',
+				deferLoading: 1,
+				columns: [
+					{
+						name: 'username',
+						data: 'username'
+					},
+					{
+						name: 'surname',
+						data: 'surname'
+					},
+					{
+						name: 'name',
+						data: 'name'
+					},
+					{
+						data: null,
+						defaultContent: '',
+						orderable: false,
+						render: function(data, type, row, meta) {
+							return '<button id="edit_' + row[0] + '"></button>';
+						},
+						targets: -1
+					}
+				],
+				searchCols: [
+					{
+						search: userSearch.get('username')
+					},
+					{
+						search: userSearch.get('surname')
+					},
+					{
+						search: userSearch.get('name')
+					},
+					null
+				],
+				language: {
+					lengthMenu: 'Mostra _MENU_ righe',
+					zeroRecords: 'Nessun risultato trovato',
+					info: 'Pagina _PAGE_ di _PAGES_',
+					infoEmpty: 'Nessun risultato trovato',
+					infoFiltered: '(filtered from _MAX_ total records)',
+					paginate: {
+						first:		'Inizio',
+						last:			'Fine',
+						next:			'Successivo',
+						previous:	'Precedente'
+					},
+					search: 'Cerca'
+				}
+			});
+			
+			// Every time the gris is redrawn, enable its buttons.
+			//
+			datatable.on('draw.dt', function() {
+				datatable.find('button').button({
+					icons: {
+						primary: 'ui-icon-document',
+						text: false
+					}
+				}).width('35px').height('20px');
+			});
+
+			// Apply search filters.
+			//
+			this.doSearch();
+
+			// Set up focus on the first search field.
+			//
+			_.defer(function () {
+				this.$('#username').focus();
+			});
+
+			return this;
+		},
+
 		ajaxSearch: function(data, callback, settings) {
 			var authorization = loginInfo.getAuthorization();
 
@@ -51,84 +145,19 @@ function ($, _, Backbone, ParentView, userSearch, loginInfo, searchHtml) {
 			});
 		},
 
-		render: function () {
-			var html = this.template();
-			this.$el.html(html);
-
-			this.$('#new').button();
-
-			var datatable = this.$('#list').dataTable({
-				serverSide: true,
-				ajax: this.ajaxSearch,
-				dom: 'tipr',
-				columns: [
-					{
-						name: 'username',
-						data: 'username'
-					},
-					{
-						name: 'surname',
-						data: 'surname'
-					},
-					{
-						name: 'name',
-						data: 'name'
-					},
-					{
-						data: null,
-						defaultContent: '',
-						orderable: false,
-						render: function(data, type, row, meta) {
-							return '<button id="edit_' + row[0] + '"></button>';
-						},
-						targets: -1
-					}
-				],
-				language: {
-					lengthMenu: 'Mostra _MENU_ righe',
-					zeroRecords: 'Nessun risultato trovato',
-					info: 'Pagina _PAGE_ di _PAGES_',
-					infoEmpty: 'Nessun risultato trovato',
-					infoFiltered: '(filtered from _MAX_ total records)',
-					paginate: {
-						first:		'Inizio',
-						last:			'Fine',
-						next:			'Successivo',
-						previous:	'Precedente'
-					},
-					search: 'Cerca'
-				}
-			});
-			
-			datatable.on('draw.dt', function() {
-				
-				// Enable grid buttons.
-				//
-				datatable.find('button').button({
-					icons: {
-						primary: 'ui-icon-document',
-						text: false
-					}
-				}).width('35px').height('20px');
-			});
-
-			// Set up focus on the first search field.
-			//
-			_.defer(function () {
-				this.$('#username').focus();
-			});
-
-			return this;
-		},
-
 		onKeyup: function() {
 			
 			// Load model from view.
 			//
-			this.fillSearchModel();
+			this.formToSearchModel();
 
 			// Execute query again.
 			//
+			this.doSearch();
+		},
+		
+		doSearch: function() {
+
 			var datatable = this.$('#list').DataTable();
 			datatable.column('username:name').search(userSearch.get('username'));
 			datatable.column('name:name').search(userSearch.get('name'));
@@ -136,11 +165,18 @@ function ($, _, Backbone, ParentView, userSearch, loginInfo, searchHtml) {
 			datatable.draw();
 		},
 
-		fillSearchModel: function() {
+		formToSearchModel: function() {
 
 			userSearch.set('username', this.$('#username').val());
 			userSearch.set('surname', this.$('#surname').val());
 			userSearch.set('name', this.$('#name').val());
+		},
+
+		searchModelToForm: function() {
+
+			this.$('#username').val(userSearch.get('username'));
+			this.$('#surname').val(userSearch.get('surname'));
+			this.$('#name').val(userSearch.get('name'));
 		}
 	});
 
