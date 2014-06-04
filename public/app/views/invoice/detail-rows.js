b843c795c694332c13c1f53a35a5d5fc0580bcc5
Dario Fabbri <dar.fabbri@gmail.com>
2014-06-04 13:58:41 +0200

    Refactored the invoice detail view, with the creation of a separate subview for the invoice rows.

---
+++ public/app/views/invoice/detail-rows.js
@@ -0,0 +1,521 @@
+define([
+	'jquery',
+	'underscore',
+	'backbone',
+	'big',
+	'collections/uoms',
+	'collections/vats',
+	'utils/validation',
+	'text!templates/invoice/detail-rows.html'
+],
+function ($, _, Backbone, Big, uoms, vats, validation, detailRowsHtml) {
+	
+	var DetailRowsView = Backbone.View.extend({
+
+		template: _.template(detailRowsHtml),
+
+		editing: null,
+
+		editableColumns: [ 1, 2, 3, 4, 6 ],
+
+		selectableColumns: [ 0 ],
+
+		events: {
+			'click #rows tbody td': 'onClickCell',
+			'click #addRow': 'onClickAddRow',
+			'click #moveUp': 'onClickMoveUp',
+			'click #moveDown': 'onClickMoveDown'
+		},
+
+		initialize: function() {
+
+			this.on('rowchanged', this.updateTotals, this);
+		},
+
+		render: function () {
+
+			var html = this.template(this.model.toJSON());
+			this.$el.html(html);
+
+			// Set up form buttons.
+			//
+			this.$('#addRow').button();
+			this.$('#moveUp').button({ disabled: true });
+			this.$('#moveDown').button({ disabled: true });
+
+			// Configure detail data table.
+			//
+			var datatable = this.$('#rows').dataTable({
+				serverSide: false,
+				data: this.model.get('detail'),
+				dom: 'tr',
+				paging: false,
+				columns: [
+					{
+						name: 'position',
+						data: 'position',
+						orderable: false
+					},
+					{
+						name: 'description',
+						data: 'description',
+						orderable: false
+					},
+					{
+						name: 'uom',
+						data: 'uom',
+						orderable: false
+					},
+					{
+						name: 'quantity',
+						data: 'quantity',
+						orderable: false
+					},
+					{
+						name: 'price',
+						data: 'price',
+						orderable: false
+					},
+					{
+						name: 'taxable',
+						data: 'taxable',
+						orderable: false
+					},
+					{
+						name: 'vatPercentage',
+						data: 'vatPercentage',
+						orderable: false
+					}
+				],
+				language: {
+					lengthMenu: 'Mostra _MENU_ righe',
+					zeroRecords: 'Nessuna riga presente',
+					info: 'Pagina _PAGE_ di _PAGES_',
+					infoEmpty: 'Nessuna riga presente',
+					infoFiltered: '',
+					paginate: {
+						first:		'Inizio',
+						last:			'Fine',
+						next:			'Successivo',
+						previous:	'Precedente'
+					},
+					search: 'Cerca'
+				}
+			});
+
+			return this;
+		},
+
+		onRemove: function() {
+
+			this.$('#addRow').button('destroy');
+			this.$('#moveUp').button('destroy');
+			this.$('#moveDown').button('destroy');
+
+			this.$('#rows').DataTable().destroy();
+		},
+
+		onClickAddRow: function() {
+
+			// Get the data table object.
+			//
+			var datatable = this.$('#rows').DataTable();
+
+			// Retrieve the last position added.
+			//
+			var lastRow = this.$('#rows tr:last');
+			var data = datatable.row(lastRow).data();
+
+			var position = (data && data.position) ? data.position + 10 : 10;
+
+			datatable.row.add({
+				position: position,
+				description: null,
+				uom: null,
+				quantity: null,
+				price: null,
+				taxable: null,
+				vatPercentage: null
+			});
+			datatable.draw();
+		},
+
+		moveRow: function(direction) {
+
+			// Check passed parameter.
+			//
+			if(!direction || (direction !== 'up' && direction != 'down')) {
+				throw "Parameter direction needs to be properly specified.";
+			}
+
+			// Find the selected table row.
+			//
+			var selected = this.$('#rows tr.selected');
+			if(!selected) {
+				return;
+			}
+
+			// Get the rows data table.
+			//
+			var datatable = this.$('#rows').DataTable();
+
+			// Get the index of the selected row.
+			//
+			var selIdx = datatable.row(selected).index();
+			if(direction === 'up' && selIdx <= 0) {
+				return;
+			}
+			if(direction === 'down' && selIdx >= datatable.data().length - 1) {
+				return;
+			}
+
+			// Swap with the row adjacent the selected one.
+			//
+			var other = direction === 'up' ? datatable.row(selIdx - 1).data() : datatable.row(selIdx + 1).data();
+			current = datatable.row(selIdx).data();
+			var tmppos = current.position;
+			current.position = other.position;
+			other.position = tmppos;
+			datatable.row(selIdx + (direction === 'up' ? -1 : 1)).data(current);
+			datatable.row(selIdx).data(other);
+
+			// Manage row selection.
+			//
+			selected.removeClass('selected');
+			if(direction === 'up') {
+				selected.prev().addClass('selected');
+			} else {
+				selected.next().addClass('selected');
+			}
+
+			// Redraw.
+			//
+			datatable.draw();
+		},
+
+		onClickMoveUp: function() {
+			this.moveRow('up');
+		},
+
+		onClickMoveDown: function() {
+			this.moveRow('down');
+		},
+
+		validateEditingCell: function() {
+
+			if(!this.editing) {
+				throw "Unexpected call to this function with no cell currently editing.";
+			}
+
+			// Extract some cell info.
+			//
+			var datatable = $(this.editing).closest('table').DataTable();
+			var cell = datatable.cell(this.editing);
+			var column = cell.index().column;
+			var val = $(this.editing).find('input').val();
+
+			// Process each column in a specialized way.
+			//
+			switch(column) {
+				
+				// Unit of measure.
+				//
+				case 2:
+					var item = uoms.find(function(item) {
+						return item.get('description') === val;
+					});
+					if(!item) {
+						return 'L\'unità di misura specificata non è valida.';
+					}
+				break;
+
+				// Quantity.
+				//
+				case 3:
+					if(!validation.isValidNumber(val)) {
+						return 'La quantità immessa non rappresenta un numero valido.';
+					}
+				break;
+
+				// Price.
+				//
+				case 4:
+					if(!validation.isValidNumber(val)) {
+						return 'Il prezzo immesso non rappresenta un numero valido.';
+					}
+				break;
+
+				// VAT percentage.
+				//
+				case 6:
+					var item = vats.find(function(item) {
+						return item.get('description') === val;
+					});
+					if(!item) {
+						return 'La percentuale IVA indicata non è valida.';
+					}
+				break;
+			}
+
+			// No validation problems detected.
+			//
+			return false;
+		},
+
+		updateTotals: function(td) {
+
+			var row = $(this.editing)
+				.closest('table')
+				.DataTable()
+				.row($(this.editing).closest('tr'));
+
+			var data = row.data();
+
+			if(data.quantity && data.price) {
+				data.taxable = Big(data.quantity.replace(',', '.'))
+					.times(data.price.replace(',', '.'))
+					.toFormat();
+				row.data(data);
+			}
+		},
+
+		resetEditCell: function() {
+
+			if(!this.editing) {
+				throw "Unexpected call to this function with no cell currently editing.";
+			}
+
+			// Access inner input widget.
+			//
+			var input = $(this.editing).find('input');
+			var data = input.val();
+
+			// Perform validation.
+			//
+			var validationError = this.validateEditingCell();
+			if(validationError) {
+
+				// Set the field error style and the tooltip.
+				//
+				input
+					.removeClass('field-error')
+					.addClass('field-error')
+					.data('hasTooltip', true)
+					.attr('title', validationError)
+					.tooltip();
+
+				return false;
+			}
+
+			// If the widget had previously been enhanced as an autocomplete,
+			// destroy it to avoid memory leaks.
+			//
+			if(input.hasClass('ui-autocomplete-input')) {
+				input.autocomplete('destroy');
+			}
+
+			// If the tooltip had been previously activated on the
+			// input field, remove it to prevent memory leaks.
+			//
+			if(input.data('hasTooltip')) {
+				input.tooltip('destroy');
+			}
+
+			// Restore the control data in the table cell content.
+			//
+			var datatable = $(this.editing)
+				.empty()
+				.closest('table')
+				.DataTable()
+				.cell(this.editing)
+				.data(data);
+
+			// Update the totals in the row.
+			//
+			this.trigger('rowchanged');
+
+			// Reset editing flag variable.
+			//
+			this.editing = null;
+
+			return true;
+		},
+
+		setEditCell: function(target) {
+
+			var that = this;
+
+		
+			// Get the cell associated to the passed target argument.
+			//
+			var cell = $(target).closest('table').DataTable().cell(target);
+
+			// Check if the selected target cell is a valid table cell.
+			//
+			if(cell.length != 1) {
+				return;
+			}
+			if(!this.isEditable(cell.index().column)) {
+				return;
+			}
+
+			// Save existing data.
+			//
+			var data = cell.data();
+
+			// Turn the cell into an input control.
+			//
+			var input = $(target)
+				.empty()
+				.append('<input type="text" style="width: 100%; height: 100%;"/>')
+				.find('input')
+				.val(data)
+				.on('blur', function(e) {
+
+					that.resetEditCell();
+				})
+				.on('keydown', function(e) {
+					if(e.keyCode === 13) {
+
+						e.preventDefault();
+						that.resetEditCell();
+
+					} else if(e.keyCode === 9) {
+
+						e.preventDefault();
+						that.tabToNext(e.currentTarget, !e.shiftKey);
+					}
+				})
+				.focus();
+
+			// Specialize the field user interface in certain cases.
+			//
+			switch(cell.index().column) {
+				case 2:
+					this.autocompletize(input, uoms);
+					break;
+
+				case 6:
+					this.autocompletize(input, vats);
+					break;
+			}
+
+			// Manage row selection.
+			//
+			var tr = $(target).closest('tr');
+			if (!tr.hasClass('selected')) {
+				tr.siblings('tr.selected').removeClass('selected');
+				tr.addClass('selected');
+				this.$('#moveUp').button('option', 'disabled', false);
+				this.$('#moveDown').button('option', 'disabled', false);
+			}
+
+			// Set editing indicator.
+			//
+			this.editing = target;
+		},
+
+		autocompletize: function(input, collection) {
+
+			input.autocomplete({
+				source: function(request, response) {
+					response(collection.list(request.term));
+				},
+				minLength: 0
+			})
+			.on('click', function(e) {
+				$(e.target).autocomplete('search');
+			});
+			input.autocomplete('search');
+		},
+
+		isEditable: function(idx) {
+			
+			return _.indexOf(this.editableColumns, idx) >= 0;
+		},
+
+		isSelectable: function(idx) {
+			
+			return _.indexOf(this.selectableColumns, idx) >= 0;
+		},
+
+		tabToNext: function (input, forward) {
+
+			var datatable = $(input).closest('table').DataTable();
+			var indexes = datatable.cell($(input).closest('td')).index();
+			var idx = indexes.column;
+
+			var limit = (forward ? _.max : _.min).call(this, this.editableColumns, function(c) { return c; });
+
+			var next = null;
+
+			while(true) {
+
+				idx += forward ? 1 : -1;
+
+				if(forward) {
+					if(idx > limit) {
+						break;
+					}
+				} else {
+					if(idx < limit) {
+						break;
+					}
+				}
+
+				if(this.isEditable(idx)) {
+					next = $(input).closest('tr').find('td:nth-child(' + (idx + 1) + ')');
+					break;
+				}
+			}
+
+			if(next) {
+				if(this.resetEditCell()) {
+					this.setEditCell(next);
+				}
+			}
+		},
+
+		onClickCell: function (e) {
+
+			var indexes = $(e.currentTarget).closest('table').DataTable().cell(e.currentTarget).index();
+
+			// Manage row selection: it only happens if the first column is clicked.
+			//
+			if(this.isSelectable(indexes.column)) {
+
+				var tr = $(e.currentTarget).closest('tr');
+
+				if (tr.hasClass('selected')) {
+					tr.removeClass('selected');
+					this.$('#moveUp').button('option', 'disabled', true);
+					this.$('#moveDown').button('option', 'disabled', true);
+				} else {
+					tr.siblings('tr.selected').removeClass('selected');
+					tr.addClass('selected');
+					this.$('#moveUp').button('option', 'disabled', false);
+					this.$('#moveDown').button('option', 'disabled', false);
+				}
+			}
+
+			// If we are already editing the clicked cell, just do nothing.
+			//
+			if(this.editing && this.editing === e.currentTarget) {
+				return;
+			}
+
+			// If the user elected to edit a different cell, the previously edited needs
+			// to be reseted.
+			//
+			if(this.editing && this.editing !== e.currentTarget) {
+				this.resetEditCell();
+			}
+
+			// If the user clicked on an editable cell, turn it in an input control.
+			//
+			this.setEditCell(e.currentTarget);
+		}
+	});
+
+	return DetailRowsView;
+});
--- public/app/views/invoice/detail.js
+++ public/app/views/invoice/detail.js
@@ -2,35 +2,22 @@ define([
 	'jquery',
 	'underscore',
 	'backbone',
-	'big',
 	'models/contact',
 	'views/form/form',
 	'views/invoice/contact-picker',
+	'views/invoice/detail-rows',
 	'collections/cities',
 	'collections/counties',
-	'collections/uoms',
-	'collections/vats',
-	'utils/validation',
 	'text!templates/invoice/detail.html'
 ],
-function ($, _, Backbone, Big, ContactModel, FormView, ContactPickerView, cities, counties, uoms, vats, validation, detailHtml) {
+function ($, _, Backbone, ContactModel, FormView, ContactPickerView, DetailRowsView, cities, counties, detailHtml) {
 	
 	var DetailView = FormView.extend({
 
 		template: _.template(detailHtml),
 
-		editing: null,
-
-		editableColumns: [ 1, 2, 3, 4, 6 ],
-
-		selectableColumns: [ 0 ],
-
 		events: {
-			'click #rows tbody td': 'onClickCell',
 			'click #selectAddressee': 'onClickSelectAddressee',
-			'click #addRow': 'onClickAddRow',
-			'click #moveUp': 'onClickMoveUp',
-			'click #moveDown': 'onClickMoveDown',
 			'click #back': 'onClickBack',
 			'click #save': 'onClickSave'
 		},
@@ -38,7 +25,6 @@ function ($, _, Backbone, Big, ContactModel, FormView, ContactPickerView, cities
 		initialize: function() {
 
 			Backbone.on('invoice:contact-picker-select', this.onContactPickerSelect, this);
-			this.on('rowchanged', this.updateTotals, this);
 		},
 
 		render: function () {
@@ -70,72 +56,12 @@ function ($, _, Backbone, Big, ContactModel, FormView, ContactPickerView, cities
 
 			// Set up the accordions.
 			//
-			this.$('#accordion-header').accordion({collapsible: true, heightStyle: 'content', active:  0});
-			this.$('#accordion-issuer').accordion({collapsible: true, heightStyle: 'content', active:  false});
-			this.$('#accordion-addressee').accordion({collapsible: true, heightStyle: 'content', active:  0});
-			this.$('#accordion-receipt').accordion({collapsible: true, heightStyle: 'content', active:  0});
-			this.$('#accordion-detail').accordion({collapsible: true, heightStyle: 'content', active:  0});
-			this.$('#accordion-totals').accordion({collapsible: true, heightStyle: 'content', active:  0});
-
-			// Configure detail data table.
-			//
-			var datatable = this.$('#rows').dataTable({
-				serverSide: false,
-				data: this.model.get('detail'),
-				dom: 'tr',
-				paging: false,
-				columns: [
-					{
-						name: 'position',
-						data: 'position',
-						orderable: false
-					},
-					{
-						name: 'description',
-						data: 'description',
-						orderable: false
-					},
-					{
-						name: 'uom',
-						data: 'uom',
-						orderable: false
-					},
-					{
-						name: 'quantity',
-						data: 'quantity',
-						orderable: false
-					},
-					{
-						name: 'price',
-						data: 'price',
-						orderable: false
-					},
-					{
-						name: 'taxable',
-						data: 'taxable',
-						orderable: false
-					},
-					{
-						name: 'vatPercentage',
-						data: 'vatPercentage',
-						orderable: false
-					}
-				],
-				language: {
-					lengthMenu: 'Mostra _MENU_ righe',
-					zeroRecords: 'Nessuna riga presente',
-					info: 'Pagina _PAGE_ di _PAGES_',
-					infoEmpty: 'Nessuna riga presente',
-					infoFiltered: '',
-					paginate: {
-						first:		'Inizio',
-						last:			'Fine',
-						next:			'Successivo',
-						previous:	'Precedente'
-					},
-					search: 'Cerca'
-				}
-			});
+			this.$('#accordion-header').accordion({collapsible: true, heightStyle: 'content', active: 0});
+			this.$('#accordion-issuer').accordion({collapsible: true, heightStyle: 'content', active: false});
+			this.$('#accordion-addressee').accordion({collapsible: true, heightStyle: 'content', active: 0});
+			this.$('#accordion-receipt').accordion({collapsible: true, heightStyle: 'content', active: 0});
+			this.$('#accordion-detail').accordion({collapsible: true, heightStyle: 'content', active: 0});
+			this.$('#accordion-totals').accordion({collapsible: true, heightStyle: 'content', active: 0});
 
 			// Set focus on the first form field.
 			//
@@ -149,6 +75,14 @@ function ($, _, Backbone, Big, ContactModel, FormView, ContactPickerView, cities
 			this.addSubview(contactPicker);
 			contactPicker.render();
 
+			// Create the invoice detail subview.
+			//
+			var detailRows = new DetailRowsView({
+				model: this.model,
+				el: this.$('#invoice-rows')
+			});
+			this.addSubview(detailRows)
+			detailRows.render();
 			return this;
 		},
 
@@ -202,9 +136,6 @@ function ($, _, Backbone, Big, ContactModel, FormView, ContactPickerView, cities
 		onRemove: function() {
 
 			this.$('#selectAddressee').button('destroy');
-			this.$('#addRow').button('destroy');
-			this.$('#moveUp').button('destroy');
-			this.$('#moveDown').button('destroy');
 			this.$('#save').button('destroy');
 			this.$('#back').button('destroy');
 
@@ -217,94 +148,6 @@ function ($, _, Backbone, Big, ContactModel, FormView, ContactPickerView, cities
 			this.$('#accordion-receipt').accordion('destroy');
 			this.$('#accordion-detail').accordion('destroy');
 			this.$('#accordion-totals').accordion('destroy');
-
-			this.$('#rows').DataTable().destroy();
-		},
-
-		onClickAddRow: function() {
-
-			// Get the data table object.
-			//
-			var datatable = this.$('#rows').DataTable();
-
-			// Retrieve the last position added.
-			//
-			var lastRow = this.$('#rows tr:last');
-			var data = datatable.row(lastRow).data();
-
-			var position = (data && data.position) ? data.position + 10 : 10;
-
-			datatable.row.add({
-				position: position,
-				description: null,
-				uom: null,
-				quantity: null,
-				price: null,
-				taxable: null,
-				vatPercentage: null
-			});
-			datatable.draw();
-		},
-
-		moveRow: function(direction) {
-
-			// Check passed parameter.
-			//
-			if(!direction || (direction !== 'up' && direction != 'down')) {
-				throw "Parameter direction needs to be properly specified.";
-			}
-
-			// Find the selected table row.
-			//
-			var selected = this.$('#rows tr.selected');
-			if(!selected) {
-				return;
-			}
-
-			// Get the rows data table.
-			//
-			var datatable = this.$('#rows').DataTable();
-
-			// Get the index of the selected row.
-			//
-			var selIdx = datatable.row(selected).index();
-			if(direction === 'up' && selIdx <= 0) {
-				return;
-			}
-			if(direction === 'down' && selIdx >= datatable.data().length - 1) {
-				return;
-			}
-
-			// Swap with the row adjacent the selected one.
-			//
-			var other = direction === 'up' ? datatable.row(selIdx - 1).data() : datatable.row(selIdx + 1).data();
-			current = datatable.row(selIdx).data();
-			var tmppos = current.position;
-			current.position = other.position;
-			other.position = tmppos;
-			datatable.row(selIdx + (direction === 'up' ? -1 : 1)).data(current);
-			datatable.row(selIdx).data(other);
-
-			// Manage row selection.
-			//
-			selected.removeClass('selected');
-			if(direction === 'up') {
-				selected.prev().addClass('selected');
-			} else {
-				selected.next().addClass('selected');
-			}
-
-			// Redraw.
-			//
-			datatable.draw();
-		},
-
-		onClickMoveUp: function() {
-			this.moveRow('up');
-		},
-
-		onClickMoveDown: function() {
-			this.moveRow('down');
 		},
 
 		onClickSelectAddressee: function() {
@@ -404,321 +247,6 @@ function ($, _, Backbone, Big, ContactModel, FormView, ContactPickerView, cities
 			this.resetFieldError('#phone');
 			this.resetFieldError('#fax');
 			this.resetFieldError('#email');
-		},
-
-		validateEditingCell: function() {
-
-			if(!this.editing) {
-				throw "Unexpected call to this function with no cell currently editing.";
-			}
-
-			// Extract some cell info.
-			//
-			var datatable = $(this.editing).closest('table').DataTable();
-			var cell = datatable.cell(this.editing);
-			var column = cell.index().column;
-			var val = $(this.editing).find('input').val();
-
-			// Process each column in a specialized way.
-			//
-			switch(column) {
-				
-				// Unit of measure.
-				//
-				case 2:
-					var item = uoms.find(function(item) {
-						return item.get('description') === val;
-					});
-					if(!item) {
-						return 'L\'unità di misura specificata non è valida.';
-					}
-				break;
-
-				// Quantity.
-				//
-				case 3:
-					if(!validation.isValidNumber(val)) {
-						return 'La quantità immessa non rappresenta un numero valido.';
-					}
-				break;
-
-				// Price.
-				//
-				case 4:
-					if(!validation.isValidNumber(val)) {
-						return 'Il prezzo immesso non rappresenta un numero valido.';
-					}
-				break;
-
-				// VAT percentage.
-				//
-				case 6:
-					var item = vats.find(function(item) {
-						return item.get('description') === val;
-					});
-					if(!item) {
-						return 'La percentuale IVA indicata non è valida.';
-					}
-				break;
-			}
-
-			// No validation problems detected.
-			//
-			return false;
-		},
-
-		updateTotals: function(td) {
-
-			var row = $(this.editing)
-				.closest('table')
-				.DataTable()
-				.row($(this.editing).closest('tr'));
-
-			var data = row.data();
-
-			if(data.quantity && data.price) {
-				data.taxable = Big(data.quantity.replace(',', '.'))
-					.times(data.price.replace(',', '.'))
-					.toFormat();
-				row.data(data);
-			}
-		},
-
-		resetEditCell: function() {
-
-			if(!this.editing) {
-				throw "Unexpected call to this function with no cell currently editing.";
-			}
-
-			// Access inner input widget.
-			//
-			var input = $(this.editing).find('input');
-			var data = input.val();
-
-			// Perform validation.
-			//
-			var validationError = this.validateEditingCell();
-			if(validationError) {
-
-				// Set the field error style and the tooltip.
-				//
-				input
-					.removeClass('field-error')
-					.addClass('field-error')
-					.data('hasTooltip', true)
-					.attr('title', validationError)
-					.tooltip();
-
-				return false;
-			}
-
-			// If the widget had previously been enhanced as an autocomplete,
-			// destroy it to avoid memory leaks.
-			//
-			if(input.hasClass('ui-autocomplete-input')) {
-				input.autocomplete('destroy');
-			}
-
-			// If the tooltip had been previously activated on the
-			// input field, remove it to prevent memory leaks.
-			//
-			if(input.data('hasTooltip')) {
-				input.tooltip('destroy');
-			}
-
-			// Restore the control data in the table cell content.
-			//
-			var datatable = $(this.editing)
-				.empty()
-				.closest('table')
-				.DataTable()
-				.cell(this.editing)
-				.data(data);
-
-			// Update the totals in the row.
-			//
-			this.trigger('rowchanged');
-
-			// Reset editing flag variable.
-			//
-			this.editing = null;
-
-			return true;
-		},
-
-		setEditCell: function(target) {
-
-			var that = this;
-
-		
-			// Get the cell associated to the passed target argument.
-			//
-			var cell = $(target).closest('table').DataTable().cell(target);
-
-			// Check if the selected target cell is a valid table cell.
-			//
-			if(cell.length != 1) {
-				return;
-			}
-			if(!this.isEditable(cell.index().column)) {
-				return;
-			}
-
-			// Save existing data.
-			//
-			var data = cell.data();
-
-			// Turn the cell into an input control.
-			//
-			var input = $(target)
-				.empty()
-				.append('<input type="text" style="width: 100%; height: 100%;"/>')
-				.find('input')
-				.val(data)
-				.on('blur', function(e) {
-
-					that.resetEditCell();
-				})
-				.on('keydown', function(e) {
-					if(e.keyCode === 13) {
-
-						e.preventDefault();
-						that.resetEditCell();
-
-					} else if(e.keyCode === 9) {
-
-						e.preventDefault();
-						that.tabToNext(e.currentTarget, !e.shiftKey);
-					}
-				})
-				.focus();
-
-			// Specialize the field user interface in certain cases.
-			//
-			switch(cell.index().column) {
-				case 2:
-					this.autocompletize(input, uoms);
-					break;
-
-				case 6:
-					this.autocompletize(input, vats);
-					break;
-			}
-
-			// Manage row selection.
-			//
-			var tr = $(target).closest('tr');
-			if (!tr.hasClass('selected')) {
-				tr.siblings('tr.selected').removeClass('selected');
-				tr.addClass('selected');
-				this.$('#moveUp').button('option', 'disabled', false);
-				this.$('#moveDown').button('option', 'disabled', false);
-			}
-
-			// Set editing indicator.
-			//
-			this.editing = target;
-		},
-
-		autocompletize: function(input, collection) {
-
-			input.autocomplete({
-				source: function(request, response) {
-					response(collection.list(request.term));
-				},
-				minLength: 0
-			})
-			.on('click', function(e) {
-				$(e.target).autocomplete('search');
-			});
-			input.autocomplete('search');
-		},
-
-		isEditable: function(idx) {
-			
-			return _.indexOf(this.editableColumns, idx) >= 0;
-		},
-
-		isSelectable: function(idx) {
-			
-			return _.indexOf(this.selectableColumns, idx) >= 0;
-		},
-
-		tabToNext: function (input, forward) {
-
-			var datatable = $(input).closest('table').DataTable();
-			var indexes = datatable.cell($(input).closest('td')).index();
-			var idx = indexes.column;
-
-			var limit = (forward ? _.max : _.min).call(this, this.editableColumns, function(c) { return c; });
-
-			var next = null;
-
-			while(true) {
-
-				idx += forward ? 1 : -1;
-
-				if(forward) {
-					if(idx > limit) {
-						break;
-					}
-				} else {
-					if(idx < limit) {
-						break;
-					}
-				}
-
-				if(this.isEditable(idx)) {
-					next = $(input).closest('tr').find('td:nth-child(' + (idx + 1) + ')');
-					break;
-				}
-			}
-
-			if(next) {
-				if(this.resetEditCell()) {
-					this.setEditCell(next);
-				}
-			}
-		},
-
-		onClickCell: function (e) {
-
-			var indexes = $(e.currentTarget).closest('table').DataTable().cell(e.currentTarget).index();
-
-			// Manage row selection: it only happens if the first column is clicked.
-			//
-			if(this.isSelectable(indexes.column)) {
-
-				var tr = $(e.currentTarget).closest('tr');
-
-				if (tr.hasClass('selected')) {
-					tr.removeClass('selected');
-					this.$('#moveUp').button('option', 'disabled', true);
-					this.$('#moveDown').button('option', 'disabled', true);
-				} else {
-					tr.siblings('tr.selected').removeClass('selected');
-					tr.addClass('selected');
-					this.$('#moveUp').button('option', 'disabled', false);
-					this.$('#moveDown').button('option', 'disabled', false);
-				}
-			}
-
-			// If we are already editing the clicked cell, just do nothing.
-			//
-			if(this.editing && this.editing === e.currentTarget) {
-				return;
-			}
-
-			// If the user elected to edit a different cell, the previously edited needs
-			// to be reseted.
-			//
-			if(this.editing && this.editing !== e.currentTarget) {
-				this.resetEditCell();
-			}
-
-			// If the user clicked on an editable cell, turn it in an input control.
-			//
-			this.setEditCell(e.currentTarget);
 		}
 	});
 
